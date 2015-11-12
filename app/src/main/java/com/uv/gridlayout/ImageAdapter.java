be28aca381fa4aa1a7291afe4a2c0736f165e529
umaparvathy <umavenkee@gmail.com>
2015-11-12 20:25:33 +0530

    Initial commit

---
+++ app/src/main/java/com/uv/gridlayout/ImageAdapter.java
@@ -0,0 +1,57 @@
+package com.uv.gridlayout;
+
+/**
+ * Created by venkatsr on 12/11/15.
+ */
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    // Constructor
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            imageView.setPadding(8, 8, 8, 8);
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.gingerbread, R.drawable.honeycomb,
+            R.drawable.icecream, R.drawable.jeyllybean,
+            R.drawable.kitkat, R.drawable.lollipop,
+    };
+}
